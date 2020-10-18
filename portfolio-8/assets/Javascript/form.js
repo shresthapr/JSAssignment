aa745d1e7feeb6a4489360b0aa516ff8a071305f
shresthapr <shresthaprajan38@gmail.com>
2020-10-18 18:04:07 +0300

    Ready to push: Finalised

---
+++ portfolio-8/assets/Javascript/form.js
@@ -0,0 +1,83 @@
+//Form Validation
+var newName = document.getElementById("user_name");
+var newEmail = document.getElementById("user_email");
+var yourPassword = document.getElementById("password");
+var passwordConfirm = document.getElementById("confirm-password");
+
+function loginVerification(x) {
+  x.preventDefault();
+  console.log("Verifying.........");
+
+  console.log("User Name: " + getUserName());
+  console.log("User Email: " + getUserEmail());
+  console.log("User Password: " + getUserPassword());
+  console.log("Repeat password: " + getUserConfirmPassword());
+
+  if (verifyName() && verifyPassword() && verifyEmail()) {
+    return (
+      (document.getElementById("id01").style.display = "none"),
+      (document.getElementById("notification").style.display = "block"),
+      window.open("http://127.0.0.1:5500/portfolio-8/myanimation.html", "_self")
+    );
+  }
+}
+
+//Get Name
+function getUserName() {
+  return $('[name="user_name"]').val();
+}
+//Verify Name
+function verifyName() {
+  var myName = getUserName();
+  if (myName !== "" && myName.indexOf(" ") < 0) {
+    return true;
+  } else {
+    document.getElementById("error1").innerHTML =
+      "Name can not be empty use space!!";
+  }
+}
+//Get Email
+function getUserEmail() {
+  return $('[name="user_email"]').val();
+}
+
+//Verify Email
+function verifyEmail() {
+  var myEmail = getUserEmail();
+  if (myEmail === "" || myEmail.indexOf(" ") > -1) {
+    document.getElementById("error2").innerHTML = "Input can not be empty!!!";
+  }
+  var atsymbol = myEmail.indexOf("@");
+  var dotsymbol = myEmail.indexOf(".");
+  if (
+    atsymbol < 1 ||
+    dotsymbol < atsymbol + 2 ||
+    dotsymbol === myEmail.length - 1
+  ) {
+    document.getElementById("error2").innerHTML = "Input valid email!!! ";
+  }
+  return true;
+}
+//Get Password
+function getUserPassword() {
+  return $('[name="password"]').val();
+}
+
+function getUserConfirmPassword() {
+  return $('[name="confirm-password"]').val();
+}
+//paaasword verification
+function verifyPassword() {
+  var myPassword = getUserPassword();
+  var repeatPassword = getUserConfirmPassword();
+  if (
+    myPassword === repeatPassword &&
+    myPassword.indexOf(" ") < 0 &&
+    myPassword.length >= 7
+  ) {
+    return true;
+  } else {
+    document.getElementById("error3").innerHTML =
+      "Password did not match. Space character not allowed!!! Min 8 Character !!! Try again!!!";
+  }
+}
--- portfolio-8/assets/Javascript/script.js
+++ portfolio-8/assets/Javascript/script.js
@@ -1,9 +1,52 @@
-const mobilemenu = () => {
-  const menu = document.querySelector("ul");
-  menu.classList.toggle("mobilemenu");
-  document.getElementsByClassName(".Tryme").style.visibility = "hidden";
-};
+(function ($) {
+  // Mobile Responsive Dropdown
+  const mobilemenu = () => {
+    const menu = document.querySelector("ul");
+    menu.classList.toggle("mobilemenu");
+    document.getElementsByClassName(".Tryme").style.visibility = "hidden";
+  };
 
-// const newDrpDown = () => {
-//   document.getElementsByClassName(".Tryme").style.visibility = "visible";
-// };
+  //popup
+
+  var modal = document.getElementById("id01");
+
+  //close popup when clicked anywhere outside modal div
+  document.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  //Show user's location
+
+  $("#location-button").click(function () {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var platform = new H.service.Platform({
+          apikey: "Oz4m4ODU37Rrb0TVb3qZqYXEpWVMoI44qKCTRD0xl2s",
+        });
+        var defaultLayers = platform.createDefaultLayers();
+        var map = new H.Map(
+          document.getElementById("mapContainer"),
+          defaultLayers.vector.normal.map,
+          {
+            zoom: 15,
+            center: {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            },
+          }
+        );
+      });
+    }
+  });
+
+  // hide map when clicked outside
+  var mapbox = document.getElementById("mapmodel");
+
+  document.addEventListener("click", function (remove) {
+    if (remove.target === mapbox) {
+      mapbox.style.display = "none";
+    }
+  });
+})(window.jQuery);
