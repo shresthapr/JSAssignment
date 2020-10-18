9bfec19b8393fb58b154b89c9afc432ff6df0646
shresthapr <shresthaprajan38@gmail.com>
2020-10-18 03:54:15 +0300

    Location: Access to user location added. Show hide in map added

--- portfolio-8/assets/Javascript/script.js
+++ portfolio-8/assets/Javascript/script.js
@@ -16,4 +16,37 @@
       modal.style.display = "none";
     }
   };
+
+  //Show user's location
+
+  $("#location-button").click(function () {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var platform = new H.service.Platform({
+          apikey: "Oz4m4ODU37Rrb0TVb3qZqYXEpWVMoI44qKCTRD0xl2s",
+        });
+        var defaultLayers = platform.createDefaultLayers();
+        var map = new H.Map(
+          document.getElementById("mapContainer"),
+          defaultLayers.vector.normal.map,
+          {
+            zoom: 15,
+            center: {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            },
+          }
+        );
+      });
+    }
+  });
+
+  // hide map when clicked outside
+  var mapbox = document.getElementById("mapmodel");
+
+  document.addEventListener("click", function (remove) {
+    if (remove.target === mapbox) {
+      mapbox.style.display = "none";
+    }
+  });
 })(window.jQuery);
